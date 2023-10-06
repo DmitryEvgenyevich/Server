0b27451379625338a8cdc3924d7c74c191d285f1
admtyd <admtyd@SRV191>
2023-10-06 21:27:54 +0200

    add new funcs & refact

---
+++ Server/Message/Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Message
+{
+    public interface IMessage
+    {
+        string Type { get; set; }
+    }
+
+    public class Response : IMessage
+    {
+
+        public string Type { get; set; } = "Response";
+
+        public string? ErrorMessage { get; set; }
+
+        public string? Data { get; set; }
+    }
+
+    public class Notification : IMessage
+    {
+        public string Type { get; set; } = "Notification";
+
+        public string? TypeOfNotification { get; set; }
+
+        public string? Data { get; set; }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,286 +1,242 @@
-﻿using System.IO;
-using System;
-using System.IO.Pipes;
-using System.Net;
-using System.Net.Http.Headers;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using static System.Net.Mime.MediaTypeNames;
-using Microsoft.VisualBasic;
-using NAudio.Wave;
+using System.Text.Json;
+using Server.Message;
 
-class Server
+namespace Server
 {
-    static BufferedWaveProvider bufferedWaveProvider;
-    static WaveOutEvent waveOut;
-
-    static Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
-    
-    static async Task Main(string[] args)
+    class Server
     {
-        await _startServerAsync();
-    }
+        static Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
 
-    static async Task _startServerAsync()
-    {
-        TcpListener serverSocket = new TcpListener(IPAddress.Any, 8000);
-        bufferedWaveProvider = new BufferedWaveProvider(new WaveFormat(8000, 16, 1));
-        waveOut = new WaveOutEvent();
-        waveOut.Init(bufferedWaveProvider);
-        try
+        static async void test(NetworkStream stream)
         {
-            serverSocket.Start();
-            Console.WriteLine("Server started. Waiting for clients...");
-
             while (true)
             {
-                TcpClient clientSocket = await serverSocket.AcceptTcpClientAsync();
-                _ = _handleClient(clientSocket);
+                string tet = Console.ReadLine();
+                //await _sendMessage(stream, tet, "Notification");
             }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error: " + ex.Message);
-        }
-        finally
-        {
-            serverSocket.Stop();
-        }
-    }
 
-    static async Task _handleClient(TcpClient clientSocket)
-    {
-        try
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Client connected");
-            
-            using (NetworkStream stream = clientSocket.GetStream())
+            await _startServerAsync();
+        }
+
+        static async Task _startServerAsync()
+        {
+            TcpListener serverSocket = new TcpListener(IPAddress.Any, 8000);
+            try
             {
-                byte[] buffer = new byte[128];
-                int bytesRead;
+                serverSocket.Start();
+                Console.WriteLine("Server started. Waiting for clients...");
 
-
-                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-                {
-                    _ = _processCommand(_convertToString(buffer, bytesRead), clientSocket);
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error: " + ex.Message);
-            string key = clients.FirstOrDefault(x => x.Value == clientSocket).Key;
-            clients.Remove(key);
-            Console.WriteLine("Client disconnected.");
-        }
-    }
-
-    static string _convertToString(byte[] buffer, int bytesRead)
-    {
-        return Encoding.ASCII.GetString(buffer, 0, bytesRead);
-    }
-
-    static async Task _sendMessage(NetworkStream stream, string message)
-    {
-        byte[] bytes = Encoding.ASCII.GetBytes(message);
-
-        await stream.WriteAsync(bytes, 0, bytes.Length);
-        await stream.FlushAsync();
-    }
-    
-    static async Task _processCommand(string command, TcpClient clientSocket)
-    {
-        
-        switch (command)
-        {
-            case "add":
-                byte[] buffer = new byte[128];
-                int bytesRead;
-                NetworkStream stream = clientSocket.GetStream();
-                await _sendMessage(stream, "send id ");
-                stream = clientSocket.GetStream();
-                bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-                string id = _convertToString(buffer, bytesRead);
-                stream = clientSocket.GetStream();
-                clients.Add(id, clientSocket);
-                await _sendMessage(stream, "you are aded");
-                break;
-            case "SignIn":
-                //_ = await signIn(clientSocket);
-                break;
-            case "signUp":
-                //_ = await signUp(clientSocket);
-                break;
-            case "forgotPass":
-                //_ = await _forgotPass(clientSocket);
-                break;
-
-            case "sendSMS":
-                //_ = await _sendSMS(clientSocket);
-                break;
-
-            case "call":
-                //byte[] buffer1 = new byte[128];
-                //int bytesRead1;
-                //NetworkStream stream1 = clientSocket.GetStream();
-                //await _sendMessage(stream1, "write to");
-                //stream1 = clientSocket.GetStream();
-                //bytesRead1 = await stream1.ReadAsync(buffer1, 0, buffer1.Length);
-                //stream1 = clientSocket.GetStream();
-                //string id1 = _convertToString(buffer1, bytesRead1);
-                //TcpClient clientTo = clients[id1];
-
-
-                NetworkStream stream3 = clientSocket.GetStream();
-
-                byte[] buffer3 = new byte[8192];
                 while (true)
                 {
-                    int bytesRead3 = stream3.Read(buffer3, 0, buffer3.Length);
-                    bufferedWaveProvider.AddSamples(buffer3, 0, bytesRead3);
-                    waveOut.Play();
+                    TcpClient clientSocket = await serverSocket.AcceptTcpClientAsync();
+                    _handleClient(clientSocket);
                 }
-                break;
-
-            case "logOut":
-                //_ = await _logOut(clientSocket);
-                break;
-
-            default:
-                break;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                serverSocket.Stop();
+            }
         }
+
+        static void deleteUserFromList(TcpClient clientSocket)
+        {
+            try
+            {
+                string key = clients.FirstOrDefault(x => x.Value == clientSocket).Key;
+                clients.Remove(key);
+            }
+            catch (Exception ex2)
+            {
+                Console.WriteLine("Error: " + ex2.Message);
+            }
+        }
+
+        static async Task waitingForReqest(NetworkStream stream, TcpClient clientSocket)
+        {
+            int bytesRead;
+            byte[] buffer = new byte[256];
+
+            try
+            {
+                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+
+                    Response response = await _processCommand(_convertToString(buffer, bytesRead), clientSocket);
+                    await _sendMessage(stream, response, "Response");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                await _sendMessage(stream, new Response { ErrorMessage = ex.Message }, "Response");
+            }
+        }
+
+        static async void _handleClient(TcpClient clientSocket)
+        {
+            try
+            {
+                Console.WriteLine("Client connected");
+
+                using (var stream = clientSocket.GetStream())
+                {
+                    Thread myThread = new Thread(() => test(stream)); // TODO Delete
+                    myThread.Start(); // TODO delete
+
+                    await waitingForReqest(stream, clientSocket);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                deleteUserFromList(clientSocket);
+            }
+            finally
+            {
+                Console.WriteLine("Client disconnected.");
+            }
+        }
+
+        static string _convertToString(byte[] buffer, int bytesRead)
+        {
+            return Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        }
+
+        static async Task _sendMessage(NetworkStream stream, IMessage message, string temp = "")
+        {
+            string json = JsonSerializer.Serialize(message, new JsonSerializerOptions { WriteIndented = true });
+
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+
+            await stream.WriteAsync(bytes, 0, bytes.Length);
+            await stream.FlushAsync();
+        }
+
+        static string tryToGetCommandFromJson(string json)
+        { 
+            JsonDocument jsonDocument = JsonDocument.Parse(json);
+            JsonElement root = jsonDocument.RootElement;
+            root.TryGetProperty("Command", out JsonElement nameElement);
+            return nameElement.ToString();
+        }
+
+        static async Task<Response> _processCommand(string json, TcpClient clientSocket)
+        {
+            try
+            {
+                string nameElement = tryToGetCommandFromJson(json);
+
+                if (nameElement.ToString() == string.Empty)
+                    return new Response { ErrorMessage = "Can not find this proparty" };
+
+                return await _findCommand(json, clientSocket, nameElement);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return new Response { ErrorMessage = ex.Message };
+            }
+        }
+
+        private static async Task<Response> _findCommand(string json, TcpClient clientSocket, string nameElement)
+        {
+            switch (nameElement)
+            {
+                case "SignIn":
+
+                    return await signIn(clientSocket, json);
+
+                case "SignUp":
+
+                    return await signUp(clientSocket, json);
+
+                case "ForgotPassword":
+
+                    return await _forgotPassword(clientSocket, json);
+
+                case "SendMessage":
+
+                    return await _sendMessage(clientSocket, json);
+
+                case "logOut":
+
+                    return _logOut(clientSocket, json);
+
+                default:
+                    return new Response { ErrorMessage = "Can not find this proparty" };
+            }
+        }
+
+        static async Task<Response> signIn(TcpClient clientSocket, string json)
+        {
+            return new Response { Data = "signIn" };
+
+        }
+
+        static async Task<Response> signUp(TcpClient clientSocket, string json)
+        {
+            return new Response { Data = "signUp" };
+        }
+
+        static async Task<Response> _forgotPassword(TcpClient clientSocket, string json)
+        {
+            return new Response { Data = "_forgotPassword" };
+        }
+
+        static async Task<Response> _sendMessage(TcpClient clientSocket, string json)
+        {
+            /* //It works
+            //byte[] buffer1 = new byte[128];
+            //int bytesRead1;
+            //NetworkStream stream1 = clientSocket.GetStream();
+            //await _sendMessage(stream1, "write to");
+            //stream1 = clientSocket.GetStream();
+            //bytesRead1 = await stream1.ReadAsync(buffer1, 0, buffer1.Length);
+            //stream1 = clientSocket.GetStream();
+            //string id1 = _convertToString(buffer1, bytesRead1);
+            //await _sendMessage(stream1, $"write to {id1}");
+
+            //byte[] buffer2 = new byte[128];
+            //int bytesRead2;
+            //NetworkStream stream2 = clientSocket.GetStream();
+            //bytesRead2 = await stream2.ReadAsync(buffer2, 0, buffer2.Length);
+
+            //string message = _convertToString(buffer2, bytesRead2);
+
+            //TcpClient clientTo = clients[id1];
+            //await _sendMessage(clientTo.GetStream(), $"message from {message}");
+            //stream2 = clientSocket.GetStream();
+            //await _sendMessage(stream2, "done");*/
+
+
+            return new Response { Data = "_sendMessage" };
+        }
+
+        static Response _logOut(TcpClient clientSocket, string json)
+        {
+            try 
+            { 
+                string key = clients.FirstOrDefault(x => x.Value == clientSocket).Key;
+                clients.Remove(key);
+                Console.WriteLine("Client disconnected.");
+                return new Response { Data = "_logOut" };
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+                return new Response { ErrorMessage = ex.Message };
+            }
+        }
+
     }
-
-    static async Task signIn(TcpClient clientSocket)
-    {
-        //byte[] buffer1 = new byte[128];
-        //int bytesRead1;
-        //NetworkStream stream1 = clientSocket.GetStream();
-        //await _sendMessage(stream1, "write to");
-        //stream1 = clientSocket.GetStream();
-        //bytesRead1 = await stream1.ReadAsync(buffer1, 0, buffer1.Length);
-        //stream1 = clientSocket.GetStream();
-        //string id1 = _convertToString(buffer1, bytesRead1);
-        //await _sendMessage(stream1, $"write to {id1}");
-
-        //byte[] buffer2 = new byte[128];
-        //int bytesRead2;
-        //NetworkStream stream2 = clientSocket.GetStream();
-        //bytesRead2 = await stream2.ReadAsync(buffer2, 0, buffer2.Length);
-
-        //string message = _convertToString(buffer2, bytesRead2);
-
-        //TcpClient clientTo = clients[id1];
-        //await _sendMessage(clientTo.GetStream(), $"message from {message}");
-        //stream2 = clientSocket.GetStream();
-        //await _sendMessage(stream2, "done");
-    }
-
-    static async Task signUp(TcpClient clientSocket)
-    {
-        //byte[] buffer1 = new byte[128];
-        //int bytesRead1;
-        //NetworkStream stream1 = clientSocket.GetStream();
-        //await _sendMessage(stream1, "write to");
-        //stream1 = clientSocket.GetStream();
-        //bytesRead1 = await stream1.ReadAsync(buffer1, 0, buffer1.Length);
-        //stream1 = clientSocket.GetStream();
-        //string id1 = _convertToString(buffer1, bytesRead1);
-        //await _sendMessage(stream1, $"write to {id1}");
-
-        //byte[] buffer2 = new byte[128];
-        //int bytesRead2;
-        //NetworkStream stream2 = clientSocket.GetStream();
-        //bytesRead2 = await stream2.ReadAsync(buffer2, 0, buffer2.Length);
-
-        //string message = _convertToString(buffer2, bytesRead2);
-
-        //TcpClient clientTo = clients[id1];
-        //await _sendMessage(clientTo.GetStream(), $"message from {message}");
-        //stream2 = clientSocket.GetStream();
-        //await _sendMessage(stream2, "done");
-    }
-
-    static async Task _forgotPass(TcpClient clientSocket)
-    {
-        //byte[] buffer1 = new byte[128];
-        //int bytesRead1;
-        //NetworkStream stream1 = clientSocket.GetStream();
-        //await _sendMessage(stream1, "write to");
-        //stream1 = clientSocket.GetStream();
-        //bytesRead1 = await stream1.ReadAsync(buffer1, 0, buffer1.Length);
-        //stream1 = clientSocket.GetStream();
-        //string id1 = _convertToString(buffer1, bytesRead1);
-        //await _sendMessage(stream1, $"write to {id1}");
-
-        //byte[] buffer2 = new byte[128];
-        //int bytesRead2;
-        //NetworkStream stream2 = clientSocket.GetStream();
-        //bytesRead2 = await stream2.ReadAsync(buffer2, 0, buffer2.Length);
-
-        //string message = _convertToString(buffer2, bytesRead2);
-
-        //TcpClient clientTo = clients[id1];
-        //await _sendMessage(clientTo.GetStream(), $"message from {message}");
-        //stream2 = clientSocket.GetStream();
-        //await _sendMessage(stream2, "done");
-    }
-
-    static async Task _sendSMS(TcpClient clientSocket)
-    {
-        //byte[] buffer1 = new byte[128];
-        //int bytesRead1;
-        //NetworkStream stream1 = clientSocket.GetStream();
-        //await _sendMessage(stream1, "write to");
-        //stream1 = clientSocket.GetStream();
-        //bytesRead1 = await stream1.ReadAsync(buffer1, 0, buffer1.Length);
-        //stream1 = clientSocket.GetStream();
-        //string id1 = _convertToString(buffer1, bytesRead1);
-        //await _sendMessage(stream1, $"write to {id1}");
-
-        //byte[] buffer2 = new byte[128];
-        //int bytesRead2;
-        //NetworkStream stream2 = clientSocket.GetStream();
-        //bytesRead2 = await stream2.ReadAsync(buffer2, 0, buffer2.Length);
-
-        //string message = _convertToString(buffer2, bytesRead2);
-
-        //TcpClient clientTo = clients[id1];
-        //await _sendMessage(clientTo.GetStream(), $"message from {message}");
-        //stream2 = clientSocket.GetStream();
-        //await _sendMessage(stream2, "done");
-    }
-
-    static async Task _call(TcpClient clientSocket)
-    {
-        //byte[] buffer1 = new byte[128];
-        //int bytesRead1;
-        //NetworkStream stream1 = clientSocket.GetStream();
-        //await _sendMessage(stream1, "write to");
-        //stream1 = clientSocket.GetStream();
-        //bytesRead1 = await stream1.ReadAsync(buffer1, 0, buffer1.Length);
-        //stream1 = clientSocket.GetStream();
-        //string id1 = _convertToString(buffer1, bytesRead1);
-        //await _sendMessage(stream1, $"write to {id1}");
-
-        //byte[] buffer2 = new byte[128];
-        //int bytesRead2;
-        //NetworkStream stream2 = clientSocket.GetStream();
-        //bytesRead2 = await stream2.ReadAsync(buffer2, 0, buffer2.Length);
-
-        //string message = _convertToString(buffer2, bytesRead2);
-
-        //TcpClient clientTo = clients[id1];
-        //await _sendMessage(clientTo.GetStream(), $"message from {message}");
-        //stream2 = clientSocket.GetStream();
-        //await _sendMessage(stream2, "done");
-    }
-
-    static async Task _logOut(TcpClient clientSocket)
-    {
-        string key = clients.FirstOrDefault(x => x.Value == clientSocket).Key;
-        clients.Remove(key);
-        Console.WriteLine("Client disconnected.");
-    }
-
 }
